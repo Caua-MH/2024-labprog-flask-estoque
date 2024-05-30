0def25387b2a8f66a351f2612aa455cecaac70f7
ALUNO <c.cornachioni@aluno.ifsp.edu.br>
2024-05-29 21:06:46 -0300

    aula 29/05

--- src/models/categoria.py
+++ src/models/categoria.py
@@ -1,8 +1,7 @@
 import uuid
-
 from sqlalchemy import String, Uuid
-from sqlalchemy.orm import Mapped, mapped_column
-from src.models.base_mixin import BasicRepositoryMixin, TimeStampMixin
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from src.models.base_mixin import (BasicRepositoryMixin, TimeStampMixin)
 
 from src.modules import db
 
@@ -15,3 +14,7 @@ class Categoria(db.Model, BasicRepositoryMixin, TimeStampMixin):
                                      default=uuid.uuid4)
     nome: Mapped[str] = mapped_column(String(128),
                                       nullable=False)
+    lista_de_produtos = relationship('Produto',
+                              back_populates='categoria',
+                              cascade='all, delete-orphan',
+                              lazy='select')
